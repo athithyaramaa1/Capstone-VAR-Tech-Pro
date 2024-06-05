005457cff57e73e7af4f4107c104e8f6299ad259
athithyaramaa1 <athithya.ramma@kalvium.community>
2024-06-05 18:45:00 +0530

    Added db.js and connected my capstone collections. Debugged code in server.js

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const colors = require("colors");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
 
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello!!!");
 });
+
+const PORT = process.env.PORT || 5004;
 app.listen(PORT, () => {
   console.log(`App started in port ${PORT}`);
 });
