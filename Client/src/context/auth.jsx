3434bcd19b170af39303a3c74c9a16c00019d2b3
athithyaramaa1 <athithya.ramma@kalvium.community>
2024-06-06 22:09:42 +0530

    Added Context API for efficient management of my App

---
+++ Client/src/context/auth.jsx
@@ -0,0 +1,37 @@
+import { useState, useContext, useEffect, createContext } from "react";
+import axios from "axios";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({
+    user: null,
+    token: "",
+  });
+
+  useEffect(() => {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${auth.token}`
+  }, [auth?.token]);
+
+  useEffect(() => {
+    const data = localStorage.getItem("auth");
+    if (data) {
+      const parsedData = JSON.parse(data);
+      setAuth({
+        ...auth,
+        user: parsedData.user,
+        token: parsedData.token,
+      });
+    }
+    //eslint-disable-next-line
+  }, []);
+  return (
+    <AuthContext.Provider value={[auth, setAuth]}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { useAuth, AuthProvider };
